59916a1a0532acc394a1e23f894ab5d7b4cac08a
Arham <iarham307"gmail.com>
2025-10-26 21:23:40 +0500

    Initial commit

--- src/api/base/index.js
+++ src/api/base/index.js
@@ -5,8 +5,10 @@ import Cookies from "js-cookie";
 
 // REST API Client (Default)
 const restApiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_REST_API_BASE_URL || "http://localhost:5000",
-  // baseURL: process.env.NEXT_PUBLIC_REST_API_BASE_URL || "http://3.145.147.170",
+  // baseURL: process.env.NEXT_PUBLIC_REST_API_BASE_URL || "http://localhost:5000",
+  baseURL:
+    process.env.NEXT_PUBLIC_REST_API_BASE_URL ||
+    "https://ribbon-backend.vercel.app",
 });
 
 restApiClient.interceptors.request.use((config) => {
